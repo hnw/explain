4f3a482588585d76c2c38d4f22f30356c3ffd978
Yoshio HANAWA <y@hnw.jp>
2016-04-09 14:50:54 +0900

    test

--- travis/ci.sh
+++ travis/ci.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 script_dir="$(cd "$(dirname "$0")"; pwd)"
+echo $script_dir
 
 git clone https://github.com/php/php-src
 cd php-src/ext
 rm -rf explain
 # Symlink causes endless loop on run-tests.php, so copy all files
-rsync -r --exclude=travis $(dirname $script_dir) ./
+rsync -r --exclude=travis "$(dirname "$script_dir")" ./
 cd ../
 ./buildconf --force
 ./configure --disable-all --enable-debug --enable-explain=static
